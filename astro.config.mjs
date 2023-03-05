cb56891218fa54582bf16fd9b8c4604c0b317da2
Matt <carlotta.matt@gmail.com>
2023-03-04 19:36:14 -0800

    chore: add outDir for builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  outDir: './dist',
   integrations: [tailwind(), solid()],
   adapter: vercel(),
   server: { port: 4000 },
